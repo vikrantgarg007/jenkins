5aa4b4e9d3b0a57227e68aaf3e62c0030ea92d7b
your_username <vikrant.garg@SW-LP08896.local>
2020-08-01 18:15:45 +0530

    adding some code

--- jenkins-shared-library/vars/buildJavascriptApp.groovy
+++ jenkins-shared-library/vars/buildJavascriptApp.groovy
@@ -1,6 +1,6 @@
 def call(Map config=[:], Closure body) {
-    go-1.14 {
-        git url: ""
+    node {
+        git url: "https://github.com/werne2j/sample-nodejs"
         stage("Install") {
             sh "npm install"
         }
